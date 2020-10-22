0973af6630f2f043c37e7caa430729c3ffd61df6
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-22 20:42:44 +0530

    [sasic] Add . Computing Employee Wage Using Switch Case

--- EmployeeWageProblemPart2/Program.cs
+++ EmployeeWageProblemPart2/Program.cs
@@ -5,6 +5,9 @@ namespace EmployeeWageProblemPart2
     class Program
     {
         const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+        const int EMPLOYEE_RATE_PER_HOUR = 20;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
@@ -12,24 +15,36 @@ namespace EmployeeWageProblemPart2
         static void Main(string[] args)
         {
             /// Computation
-            Console.WriteLine(EmployeeCheck());
+            Console.WriteLine("Employee Daily Wage is :: " + EmployeeCheck());
         }
-        public static String EmployeeCheck()
+        public static int EmployeeCheck()
         {
+            /// variable
+            int empHrs = 0;
+
             /// Create References For Random Class
             Random random = new Random();
-
             /// It Will Give Random Value
-            int employeeCheck = random.Next(0, 2);
+            int employeeCheck = random.Next(0, 3);
 
-            /// Checks If Employee is Present, It will return Present Message
-            /// If Employee is Not Present, It will return Absent Message
-            if (employeeCheck == IS_EMPLOYEE_FULL_TIME)
+            /// By Using Switch Case
+            /// Checks If Employee is Full Time, It will give Full time Working hours
+            /// If Employee is Part Time, It will give Part time Working hours
+            switch(employeeCheck)
             {
-                return "Employee is Present";
+                case IS_EMPLOYEE_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_EMPLOYEE_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            
-            return "Employee is Absent"; 
+
+            /// It will returns a Daily Wage
+            return empHrs * EMPLOYEE_RATE_PER_HOUR;
         }
     }
 }
