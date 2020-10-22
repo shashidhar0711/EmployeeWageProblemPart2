9c879975af022f84bfc37ecea193d2886b378f15
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-22 21:02:35 +0530

    [sasic] Add . Computing Total Employee Wages for Month

--- EmployeeWageProblemPart2/Program.cs
+++ EmployeeWageProblemPart2/Program.cs
@@ -5,6 +5,10 @@ namespace EmployeeWageProblemPart2
     class Program
     {
         const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+        const int EMPLOYEE_RATE_PER_HOUR = 20;
+        const int NUM_0F_WORKING_DAYS = 20;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
@@ -12,24 +16,42 @@ namespace EmployeeWageProblemPart2
         static void Main(string[] args)
         {
             /// Computation
-            Console.WriteLine(EmployeeCheck());
+            Console.WriteLine("Total Employee Wage is :: " + EmployeeCheck());
         }
-        public static String EmployeeCheck()
+        public static int EmployeeCheck()
         {
-            /// Create References For Random Class
-            Random random = new Random();
+            /// variable
+            int empHrs = 0, empWage = 0, totalEmployeeWage = 0;
 
-            /// It Will Give Random Value
-            int employeeCheck = random.Next(0, 2);
-
-            /// Checks If Employee is Present, It will return Present Message
-            /// If Employee is Not Present, It will return Absent Message
-            if (employeeCheck == IS_EMPLOYEE_FULL_TIME)
+            for (int day = 0; day < NUM_0F_WORKING_DAYS; day++)
             {
-                return "Employee is Present";
+                /// Create References For Random Class
+                Random random = new Random();
+                /// It Will Give Random Value
+                int employeeCheck = random.Next(0, 3);
+
+                /// By Using Switch Case
+                /// Checks If Employee is Full Time, It will give Full time Working hours
+                /// If Employee is Part Time, It will give Part time Working hours
+                switch (employeeCheck)
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs* EMPLOYEE_RATE_PER_HOUR;
+                totalEmployeeWage = totalEmployeeWage + empWage;
             }
-            
-            return "Employee is Absent"; 
+            /// It will returns a Total Employee Wage
+            return totalEmployeeWage;
         }
+            
+                    
     }
 }
