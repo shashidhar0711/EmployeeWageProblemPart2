c52dca7ebd25829b6a55400afe64ea50562819f4
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-22 21:43:11 +0530

    [sasic] Add . Computing a Total Employee Wages Till a Condition

--- EmployeeWageProblemPart2/Program.cs
+++ EmployeeWageProblemPart2/Program.cs
@@ -5,6 +5,11 @@ namespace EmployeeWageProblemPart2
     class Program
     {
         const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+        const int EMPLOYEE_RATE_PER_HOUR = 20;
+        const int NUM_0F_WORKING_DAYS = 20;
+        const int MAX_HOURS_IN_MONTH =100;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
@@ -12,24 +17,41 @@ namespace EmployeeWageProblemPart2
         static void Main(string[] args)
         {
             /// Computation
-            Console.WriteLine(EmployeeCheck());
+            Console.WriteLine("Total Employee Wages :: " + EmployeeCheck());
         }
-        public static String EmployeeCheck()
+        public static int EmployeeCheck()
         {
-            /// Create References For Random Class
-            Random random = new Random();
+            /// variable
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWokingDays = 0;
 
-            /// It Will Give Random Value
-            int employeeCheck = random.Next(0, 2);
-
-            /// Checks If Employee is Present, It will return Present Message
-            /// If Employee is Not Present, It will return Absent Message
-            if (employeeCheck == IS_EMPLOYEE_FULL_TIME)
+            /// By Looping Untill Maximum Condition Reached
+            while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWokingDays <= NUM_0F_WORKING_DAYS)
             {
-                return "Employee is Present";
+                /// Create References For Random Class
+                Random random = new Random();
+
+                /// By Using Switch Case
+                /// Checks If Employee is Full Time, It will give Full time Working hours
+                /// If Employee is Part Time, It will give Part time Working hours
+                switch (random.Next(0, 3))
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                totalWokingDays++;
             }
-            
-            return "Employee is Absent"; 
+            /// It will returns a Total Employee Wage
+            return totalEmpHrs * EMPLOYEE_RATE_PER_HOUR;
         }
     }
 }
