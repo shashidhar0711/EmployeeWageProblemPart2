cd04fa3209980f71b59d1ce340f3277d398034f1
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-25 18:26:33 +0530

    [sasic] Add . Calculating Multiple Company  Employee Wages

---
+++ EmployeeWageProblemPart2/Class1.cs
@@ -0,0 +1,69 @@
+﻿/*
+ *  C# CODING STANDARDS - LINE SPACE
+ *  NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+ */
+
+/******************************************************************************
+ *  Compilation:  C:\Users\Theja\source\repos\EmployeeWageProblemPart2\bin\Debug\netcoreapp3.1\EmployeeWageProblemPart.exe
+ *  Execution:    EmployeeWageProblemPart.exe
+ *  
+ *  Purpose: Computes Employee Wages.
+ *
+ *  @author  Shashidhar c
+ *  @version 1.0
+ *  @since   18-10-2020
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
+
+namespace EmployeeWageProblemPart
+{
+    class Program
+    {
+        const int IS_EMPLOYEE_PART_TIME = 1;
+        const int IS_EMPLOYEE_FULL_TIME = 2;
+
+        static void main(String[] args)
+        {
+            /// Monthly Wage for multiple compani
+            /// calls the Computwage method with the following parameters.
+            Console.WriteLine("Employee Wage for Samsung is : " + ComputeEmpWage("Samsung", 25, 24, 90));
+            Console.WriteLine("Employee Wage for Nokia is : " + ComputeEmpWage("Nokia", 22, 22, 100));
+        }
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        /// <param name="companyName">Name of the company.</param>
+        /// <param name="employeeRatePerHour">The employee rate per hour.</param>
+        /// <param name="numberOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursInMonth">The maximum hours in month.</param>
+        /// <returns></returns>
+        public static int ComputeEmpWage(String companyName, int employeeRatePerHour, int numberOfWorkingDays, int maxHoursInMonth)
+        {
+            int empHours = 0;
+            int totalEmpHours = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHours < maxHoursInMonth && totalWorkingDays < numberOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                switch (random.Next(0, 3))
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                totalEmpHours += empHours;
+            }
+            return totalEmpHours * employeeRatePerHour;
+        }
+    }
+}
--- EmployeeWageProblemPart2/Program.cs
+++ EmployeeWageProblemPart2/Program.cs
@@ -5,31 +5,64 @@ namespace EmployeeWageProblemPart2
     class Program
     {
         const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+        const int EMPLOYEE_RATE_PER_HOUR = 20;
+        const int NUM_0F_WORKING_DAYS = 20;
+        const int MAX_HOURS_IN_MONTH = 100;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            /// Computation
-            Console.WriteLine(EmployeeCheck());
+            ///Monthly Wage for multiple companies.
+            ///calls the Computwage method with the following parameters.
+            Console.WriteLine("Employee Wage for Samsung is : " + EmployeeCheck("Samsung", 25, 24, 90));
+            Console.WriteLine("Employee Wage for Nokia is : " + EmployeeCheck("Nokia", 22, 22, 100));
         }
-        public static String EmployeeCheck()
+
+        /// <summary>
+        /// Employees the check.
+        /// </summary>
+        /// <param name="companyName">Name of the company.</param>
+        /// <param name="employeeRatePerHour">The employee rate per hour.</param>
+        /// <param name="numberOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursInMonth">The maximum hours in month.</param>
+        /// <returns></returns>
+        public static int EmployeeCheck(String companyName, int employeeRatePerHour, int numberOfWorkingDays, int maxHoursInMonth)
         {
-            /// Create References For Random Class
-            Random random = new Random();
+            /// variable
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWokingDays = 0;
 
-            /// It Will Give Random Value
-            int employeeCheck = random.Next(0, 2);
-
-            /// Checks If Employee is Present, It will return Present Message
-            /// If Employee is Not Present, It will return Absent Message
-            if (employeeCheck == IS_EMPLOYEE_FULL_TIME)
+            /// By Looping Untill Maximum Condition Reached
+            while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWokingDays <= NUM_0F_WORKING_DAYS)
             {
-                return "Employee is Present";
+                /// Create References For Random Class
+                Random random = new Random();
+
+                /// By Using Switch Case
+                /// Checks If Employee is Full Time, It will give Full time Working hours
+                /// If Employee is Part Time, It will give Part time Working hours
+                switch (random.Next(0, 3))
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                totalWokingDays++;
             }
-            
-            return "Employee is Absent"; 
+            /// It will returns a Total Employee Wage
+            return totalEmpHrs * EMPLOYEE_RATE_PER_HOUR;
         }
     }
 }
